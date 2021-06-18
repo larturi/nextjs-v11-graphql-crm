e4e509e6f1cc506a38c031d340c636db83bb81a5
lea.arturi <lea.arturi@gmail.com>
2021-06-18 08:46:22 -0300

    Instalacion de tailwind, Layout y paginas basicas

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+
+import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
     return (
         <>
-           <h1>Desde Layout</h1> 
-           { children }
+            <Head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <title>CRM - Administración de Clientes</title>
+            </Head>
+
+            <div className="bg-gray-200 min-h-screen">
+                <div className="sm:flex min-h-screen">
+                    <Sidebar />
+
+                    <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+                        { children }
+                    </main>
+                </div>
+            </div>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 import Layout from '../components/Layout';
 
-export default function Index() {
+const Index = () => {
   return (
     <div>
       <Layout>
-        <h2>Desde Index</h2>
+        <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
       </Layout>
     </div>
   )
-}
+};
+
+export default Index;
