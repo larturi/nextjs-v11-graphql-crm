5adecec7af06a02760f32cf8466a9d36144e5163
lea.arturi <lea.arturi@gmail.com>
2021-06-25 08:50:58 -0300

    Edicion de Clientes Finalizada

--- pages/editarcliente/[id].js
+++ pages/editarcliente/[id].js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import Swal from 'sweetalert2';
 
 import Layout from '../../components/Layout';
 
@@ -13,10 +16,26 @@ const OBTENER_CLIENTE = gql`
             apellido
             email
             empresa
-            vendedor
+            vendedor,
+            telefono
         }
     }
 `;
+
+const ACTUALIZAR_CLIENTE = gql`
+    mutation actualizarCliente($id: ID!, $input: ClienteInput) {
+        actualizarCliente(id: $id, input: $input) {
+            id
+            nombre
+            apellido
+            email
+            empresa
+            vendedor
+            telefono
+        }
+    }
+`;
+
 
 const EditarCliente = () => {
 
@@ -30,136 +49,211 @@ const EditarCliente = () => {
         }
     });
 
-    if (loading) return 'Cargando...';
+    // Actualizar Cliente
+    const [ actualizarCliente ] = useMutation(ACTUALIZAR_CLIENTE);
 
-    return (
-        <Layout>
-            <h1 className="text-2xl text-gray-800 font-light">Editar Cliente</h1>
+    // Validaciones Formulario
+    const schemaValidacion = Yup.object({
+        nombre: Yup.string()
+                   .required('El nombre es obligatorio'),
+        apellido: Yup.string()
+                   .required('El apellido es obligatorio'),
+        empresa: Yup.string()
+                   .required('El nombre de la empresa es obligatorio'), 
+        email: Yup.string()
+                   .required('El email es obligatorio')
+                   .email('Debe ingresar un email valido'),
+        telefono: Yup.string()
+                   .required('El telefono es obligatorio')
+                   .min(8, 'Minimo 8 caracteres')    
+    });
 
-            <div className="flex justify-center mt-5">
-                <div className="w-full max-w-lg">
+    // Actualiza Cliente en BD
+    const actualizarInfoCliente = async valores => {
 
-                    <form 
-                        className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
-                        // onSubmit={formik.handleSubmit}
-                    >
+        const { nombre, apellido, empresa, email, telefono } = valores;
 
-                        <div className="mt-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
-                                Nombre
-                            </label>
-                            <input 
-                                className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
-                                id="nombre"
-                                type="text"
-                                placeholder="Nombre"
-                                // onChange={formik.handleChange}
-                                // onBlur={formik.handleBlur}
-                                // value={formik.values.nombre}
-                            />
-                        </div>
+        try {
+            const data = await actualizarCliente({
+                variables: {
+                    id,
+                    input: {
+                        nombre,
+                        apellido, 
+                        empresa, 
+                        email, 
+                        telefono
+                    }
+                }
+            });
 
-                        {/* { formik.touched.nombre &&formik.errors.nombre ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
-                                    <p>{formik.errors.nombre}</p>
-                                </div>
-                            ) : null } */}
+            Swal.fire(
+                'Actualizado!',
+                'El cliente ha sido actualizado correctamente',
+                'success'
+            );
 
-                        <div className="mt-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
-                                Apellido
-                            </label>
-                            <input 
-                                className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
-                                id="apellido"
-                                type="text"
-                                placeholder="Apellido"
-                                // onChange={formik.handleChange}
-                                // onBlur={formik.handleBlur}
-                                // value={formik.values.apellido}
-                            />
-                        </div>
+            router.push('/');
+        } catch (error) {
+            console.error(error);
+        }
 
-                        {/* { formik.touched.apellido &&formik.errors.apellido ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
-                                    <p>{formik.errors.apellido}</p>
-                                </div>
-                            ) : null } */}
+    };
 
-                        <div className="mt-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="empresa">
-                                Empresa
-                            </label>
-                            <input 
-                                className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
-                                id="empresa"
-                                type="text"
-                                placeholder="Empresa"
-                                // onChange={formik.handleChange}
-                                // onBlur={formik.handleBlur}
-                                // value={formik.values.empresa}
-                            />
-                        </div>
+    if (loading) {
+        return 'Cargando...';
+    } else {
 
-                        {/* { formik.touched.empresa &&formik.errors.empresa ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
-                                    <p>{formik.errors.empresa}</p>
-                                </div>
-                            ) : null } */}
+        const { obtenerCliente } = data;
 
-                        <div className="mt-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                                Email
-                            </label>
-                            <input 
-                                className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
-                                id="email"
-                                type="email"
-                                placeholder="Email"
-                                // onChange={formik.handleChange}
-                                // onBlur={formik.handleBlur}
-                                // value={formik.values.email}
-                            />
-                        </div>
+        return (
+            <Layout>
+                <h1 className="text-2xl text-gray-800 font-light">Editar Cliente</h1>
 
-                        {/* { formik.touched.email &&formik.errors.email ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
-                                    <p>{formik.errors.email}</p>
-                                </div>
-                            ) : null } */}
+                <div className="flex justify-center mt-5">
+                    <div className="w-full max-w-lg">
 
-                        <div className="mt-3">
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
-                                Teléfono
-                            </label>
-                            <input 
-                                className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
-                                id="telefono"
-                                type="tel"
-                                placeholder="Teléfono"
-                                // onChange={formik.handleChange}
-                                // onBlur={formik.handleBlur}
-                                // value={formik.values.telefono}
-                            />
-                        </div>
+                        <Formik
+                            validationSchema={schemaValidacion}
+                            enableReinitialize
+                            initialValues={obtenerCliente}
+                            onSubmit={ (values) => {
+                                actualizarInfoCliente(values);
+                            }}
+                        >
+                            {
+                                
+                                props => {
 
-                        {/* { formik.touched.telefono &&formik.errors.telefono ? (
-                                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
-                                    <p>{formik.errors.telefono}</p>
-                                </div>
-                            ) : null } */}
+                                    return (
 
-                        <input 
-                            type="submit"
-                            className="bg-gray-800 w-full mt-5 p-2 cursor-pointer text-white uppercase hover:bg-gray-900"
-                            value="Registrar Cliente"
-                        />
+                                        <form 
+                                            className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                                            onSubmit={props.handleSubmit}
+                                        >
 
-                    </form>
+                                            <div className="mt-3">
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                                                    Nombre
+                                                </label>
+                                                <input 
+                                                    className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
+                                                    id="nombre"
+                                                    type="text"
+                                                    placeholder="Nombre"
+                                                    onChange={props.handleChange}
+                                                    onBlur={props.handleBlur}
+                                                    value={props.values.nombre}
+                                                />
+                                            </div>
+
+                                            { props.touched.nombre &&props.errors.nombre ? (
+                                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
+                                                        <p>{props.errors.nombre}</p>
+                                                    </div>
+                                                ) : null }
+
+                                            <div className="mt-3">
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
+                                                    Apellido
+                                                </label>
+                                                <input 
+                                                    className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
+                                                    id="apellido"
+                                                    type="text"
+                                                    placeholder="Apellido"
+                                                    onChange={props.handleChange}
+                                                    onBlur={props.handleBlur}
+                                                    value={props.values.apellido}
+                                                />
+                                            </div>
+
+                                            { props.touched.apellido &&props.errors.apellido ? (
+                                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
+                                                        <p>{props.errors.apellido}</p>
+                                                    </div>
+                                                ) : null }
+
+                                            <div className="mt-3">
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="empresa">
+                                                    Empresa
+                                                </label>
+                                                <input 
+                                                    className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
+                                                    id="empresa"
+                                                    type="text"
+                                                    placeholder="Empresa"
+                                                    onChange={props.handleChange}
+                                                    onBlur={props.handleBlur}
+                                                    value={props.values.empresa}
+                                                />
+                                            </div>
+
+                                            { props.touched.empresa &&props.errors.empresa ? (
+                                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
+                                                        <p>{props.errors.empresa}</p>
+                                                    </div>
+                                                ) : null }
+
+                                            <div className="mt-3">
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                                    Email
+                                                </label>
+                                                <input 
+                                                    className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
+                                                    id="email"
+                                                    type="email"
+                                                    placeholder="Email"
+                                                    onChange={props.handleChange}
+                                                    onBlur={props.handleBlur}
+                                                    value={props.values.email}
+                                                />
+                                            </div>
+
+                                            { props.touched.email &&props.errors.email ? (
+                                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
+                                                        <p>{props.errors.email}</p>
+                                                    </div>
+                                                ) : null }
+
+                                            <div className="mt-3">
+                                                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
+                                                    Teléfono
+                                                </label>
+                                                <input 
+                                                    className="appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:border-gray-600"
+                                                    id="telefono"
+                                                    type="tel"
+                                                    placeholder="Teléfono"
+                                                    onChange={props.handleChange}
+                                                    onBlur={props.handleBlur}
+                                                    value={props.values.telefono}
+                                                />
+                                            </div>
+
+                                            { props.touched.telefono &&props.errors.telefono ? (
+                                                    <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-3">
+                                                        <p>{props.errors.telefono}</p>
+                                                    </div>
+                                                ) : null }
+
+                                            <input 
+                                                type="submit"
+                                                className="bg-gray-800 w-full mt-5 p-2 cursor-pointer text-white uppercase hover:bg-gray-900"
+                                                value="Actualizar Cliente"
+                                            />
+
+                                        </form>
+
+                                    )
+                                }
+                            }
+                        </Formik>
+                    </div>
                 </div>
-            </div>
-        </Layout>
-    )
+            </Layout>
+        )
+    }
 };
 
 export default EditarCliente;
