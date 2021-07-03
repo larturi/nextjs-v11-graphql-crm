ad39c7f906ad7ed4b3683ea518cb886276862f82
lea.arturi <lea.arturi@gmail.com>
2021-07-03 08:53:00 -0300

    Mejores Vendedores

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -29,6 +29,23 @@ const Sidebar = () => {
                     </Link>
                 </li>
             </nav>
+
+            <div className="sm:mt-10">
+                <p className="text-white text-2xl font-black">Otras Opciones</p>
+            </div>
+
+            <nav className="mt-6 list-none">
+                <li className={router.pathname === '/mejoresvendedores' ? "bg-indigo-600 p-2" : "p-2"}>
+                    <Link href="/mejoresvendedores">
+                        <a className="text-white mb-2 mt-2 block">Mejores Vendedores</a>
+                    </Link>
+                </li>
+                <li className={router.pathname === '/mejoresclientes' ? "bg-indigo-600 p-2" : "p-2"}>
+                    <Link href="/mejoresclientes">
+                        <a className="text-white mb-2 mt-2 block">Mejores Clientes</a>
+                    </Link>
+                </li>
+            </nav>
         </aside>
     )
 }
